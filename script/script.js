25fa7213b7a850f13facbf062b3f2b6dad2bcfd1
Nadia Nechuiviter <nadiaejsmont@gmail.com>
2022-01-25 13:44:22 +0200

    tabs

--- script/script.js
+++ script/script.js
@@ -1,12 +1,16 @@
 // document.querySelector('.header-menu__logo').style.border = '1px solid red';
+/*https://www.npmjs.com/package/seamless-scroll-polyfill*/
+
 import { elementScrollIntoView } from "seamless-scroll-polyfill";
 
 import { polyfill } from "seamless-scroll-polyfill";
 
 polyfill();
 const links = document.querySelectorAll('.header-menu__item a');
+const characteristic = document.querySelector('.card-details__link-characteristics');
 
-links.forEach((element) => {
+const newArray = [...links, characteristic];
+newArray.forEach((element) => {
     console.log(element)
     element.addEventListener('click', (event) => {
         event.preventDefault();
